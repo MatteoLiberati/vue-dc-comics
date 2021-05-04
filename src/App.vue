df7b7e037f74708dc7f88d7f818fcfb15cb3a73a
MatteoLiberati <matteoliberati@hotmail.com>
2021-05-04 15:50:09 +0200

    header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- HEADER -->
+    <Header/>
+    <!-- MAIN -->
+    <main></main>
+
+    <!-- FOOTER -->
+    <footer></footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "@/components/Header.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app{
+  background-color:black;
+  padding: 30px 0;
+  font-family: 'Roboto', sans-serif;
+}
+
+.container{
+  max-width: 1270px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,114 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="@/assets/images/dc-logo.png" :alt="logo">
+            </div>
+            <nav>
+                <ul>
+                    <li v-for="(element,index) in menu" :key="index">
+                        <a :href="element.path">{{ element.name}}</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    data(){
+        return{
+            menu : [
+                {
+                    name: "character",
+                    path: "character.dc",
+                },
+                {
+                    name: "comics",
+                    path: "comics.dc",
+                },
+                {
+                    name: "movies",
+                    path: "movies.dc",
+                },
+                {
+                    name: "tv",
+                    path: "tv.dc",
+                },
+                {
+                    name: "games",
+                    path: "games.dc",
+                },
+                {
+                    name: "collectibles",
+                    path: "collectibles.dc",
+                },
+                {
+                    name: "videos",
+                    path: "videos.dc",
+                },
+                {
+                    name: "fans",
+                    path: "fans.dc",
+                },
+                {
+                    name: "news",
+                    path: "news.dc",
+                },
+                {
+                    name: "shop",
+                    path: "shop.dc",
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+header{
+    background-color: #fff;
+}
+
+header .logo{
+    display:flex;
+    align-items: center;
+}
+
+header .logo img{
+    width: 80px;
+}
+
+header .container{
+    display: flex;
+    justify-content: space-between;
+}
+
+ul{ 
+    display: flex;
+    list-style: none;
+}
+
+ul li a{ 
+    padding: 15px;
+    text-decoration: none;
+    color: #000;
+    text-transform: uppercase;
+    font-size: .8rem;
+    font-weight: bold;
+}
+
+ul li{
+    border-bottom: 5px solid transparent;
+    padding: 50px 0;
+    transition: border-bottom .3s;
+}
+
+ul li:hover{
+    border-bottom: 5px solid #0c7cec;
+}
+
+
+</style>
